de1475b8165f082332433b7e430e5e6c707d2f4b
gszp1 <grzybekmateusz@poczta.fm>
2024-04-01 00:22:55 +0200

    Adds Employee entity class.

---
+++ src/main/java/org/example/zadania_lab1/model/Employee.java
@@ -0,0 +1,23 @@
+package org.example.zadania_lab1.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 20)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 30)
+    private String lastName;
+
+    @Column(nullable = false)
+    private BigDecimal salary;
+}
