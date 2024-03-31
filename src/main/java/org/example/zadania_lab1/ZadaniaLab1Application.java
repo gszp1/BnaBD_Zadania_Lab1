121800bfae9f765bdf7651ae37acac6ea18db57a
gszp1 <grzybekmateusz@poczta.fm>
2024-03-31 15:48:44 +0200

    Initial commit.

---
+++ src/main/java/org/example/zadania_lab1/ZadaniaLab1Application.java
@@ -0,0 +1,13 @@
+package org.example.zadania_lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadaniaLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZadaniaLab1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/zadania_lab1/ZadaniaLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.zadania_lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadaniaLab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
